13e0e7e0bf4c1752f0b047c601527cdf734d5fd0
Doosan <doosan@hartmann-controls.com>
2024-01-19 07:36:59 -0500

    Adding project plugin

---
+++ lua/plugins/project.lua
@@ -0,0 +1,13 @@
+return {
+  "ahmedkhalf/project.nvim",
+  config = function()
+    pcall(require('telescope').load_extension, 'projects')
+    require("project_nvim").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "*.sln" },
+
+    }
+  end
+}
